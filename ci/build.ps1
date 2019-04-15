5282c95fa9346592001bc8be87d4289fb185139b
Ashley Mannix <ashleymannix@live.com.au>
2019-04-16 02:22:37 +1000

    more appveyor

--- ci/build.ps1
+++ ci/build.ps1
@@ -2,6 +2,8 @@ $ErrorActionPreference = "Stop"
 
 Push-Location "$PSScriptRoot/../"
 
+. "./ci/common.ps1"
+
 Run-Command -Exe cargo -ArgumentList 'build', '--target x86_64-pc-windows-msvc'
 Run-Command -Exe cargo -ArgumentList 'build', '--target x86_64-unknown-linux-musl'
 
