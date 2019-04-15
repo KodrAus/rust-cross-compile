8ea292ca09c4bb03d9f28ff66c752c287d92346e
Ashley Mannix <ashleymannix@live.com.au>
2019-04-16 02:13:23 +1000

    work around old Powershell stderr

--- ci/build.ps1
+++ ci/build.ps1
@@ -1,8 +1,33 @@
 $ErrorActionPreference = "Stop"
 
+function Run-Command
+{
+    Param ($Exe, $ArgumentList)
+
+    # For commands that treat stderr like stdetc
+    $out = New-TemporaryFile
+    $err = New-TemporaryFile
+    $r = Start-Process $Exe -ArgumentList $ArgumentList -Wait -PassThru -RedirectStandardOut $out.FullName -RedirectStandardError $err.FullName
+
+    Write-Output "STDOUT"
+    Get-Content -Path $out.FullName
+    Write-Output ""
+
+    Write-Output "STDERR"
+    Get-Content -Path $err.FullName
+    Write-Output ""
+    
+    Remove-Item $out.FullName
+    Remove-Item $err.FullName
+
+    if ($r.ExitCode -ne 0) {
+        exit $r.ExitCode
+    }
+}
+
 Push-Location "$PSScriptRoot/../"
 
-cargo build --target x86_64-pc-windows-msvc
-cargo build --target x86_64-unknown-linux-musl
+Run-Command -Exe cargo -ArgumentList 'build', '--target x86_64-pc-windows-msvc'
+Run-Command -Exe cargo -ArgumentList 'build', '--target x86_64-unknown-linux-musl'
 
 Pop-Location
