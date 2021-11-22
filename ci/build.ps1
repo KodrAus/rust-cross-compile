d09d4b4036d12b13d1dcc3bd734dd02a973c1677
Ashley Mannix <kodraus@hey.com>
2021-11-22 12:32:37 +1000

    build for ARM

--- ci/build.ps1
+++ ci/build.ps1
@@ -6,5 +6,6 @@ Push-Location "$PSScriptRoot/../"
 
 Run-Command -Exe cargo -ArgumentList 'build', '--target x86_64-pc-windows-msvc'
 Run-Command -Exe cargo -ArgumentList 'build', '--target x86_64-unknown-linux-musl'
+Run-Command -Exe cargo -ArgumentList 'build', '--target aarch64-unknown-linux-musl'
 
 Pop-Location
